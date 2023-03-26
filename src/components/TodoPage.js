82680faf6ae6e8a3642b7e6977acbf3f8b3369de
Lorraineken <lorrainekupa@gmail.com>
2023-03-26 10:23:41 +0300

    Added sample todos

--- src/components/TodoPage.js
+++ src/components/TodoPage.js
@@ -11,13 +11,27 @@ function TodoPage({users}){
         )
     })
 
-    
+    const sample_todos = ["Do my laundry","Grocery shopping","make my hair","Finish code challenge"]
+
+    const display_todos =sample_todos.map((item) => 
+    {
+        return(
+        <div className="mt-3" key={`${item.index}${item}`}>
+         <ul >
+         <li> {item} </li>
+        </ul>   
+         
+        <button className="btn btn-dark">Update</button>
+         <button className="btn btn-danger ms-2" >x</button>
+        </div>
+        )
+    })
 
     return(
         <div>
          {displayuser}
          <h2>TODO LIST</h2>
-
+          {display_todos}
         </div>
     ) 
 }
